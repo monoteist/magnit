b397cec085441550ca877bdebb4701f7f2d91713
= <i.ezerbaev97@gmail.com>
2021-11-20 22:27:26 +0300

    add post method for api | add README.md

--- house/house_api.py
+++ house/house_api.py
@@ -1,21 +1,35 @@
 from house import api
 from flask_restful import Resource, reqparse
+from house.models import House, db
 
 
 class HouseAPI(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument('latitude', type=float, required=True,
-                                   help='No latitude provided', location='json')
+                                   help='Отсутвует значение поля latitude',
+                                   location='json')
         self.reqparse.add_argument('longitude', type=float, required=True,
-                                   help='No longitude provided', location='json')
+                                   help='Отсутвует значение поля provided',
+                                   location='json')
         self.reqparse.add_argument('family_count', type=int, required=True,
-                                   help='No family_count provided', location='json')
-        super(HouseAPI, self).__init__()
+                                   help='Отсутвует значение поля provided',
+                                   location='json')
 
     def post(self, house_id):
         args = self.reqparse.parse_args()
-        return {house_id: args}
+        house = House.query.get(house_id)
+        if house:
+            house.latitude = args.latitude
+            house.longitude = args.longitude
+            house.family_count = args.family_count
+            db.session.commit()
+            return {'message': 'Данные дома обновлены!'}
+        else:
+            house = House(**args)
+            db.session.add(house)
+            db.session.commit()
+            return {'message': 'Новый дом добавлен с переданными данными!'}
 
 
 api.add_resource(HouseAPI, '/api/v1/houses/<int:house_id>',
--- house/tests.py
+++ house/tests.py
@@ -2,7 +2,9 @@ from flask import json
 import requests
 
 BASE = 'http://127.0.0.1:5000/'
-
-response = requests.post(BASE + 'api/v1/houses/1', json={'latitude': 12.5, 'longitude':3.45, 'family_count':4})
-
+# Обновление объекта (Существующих данных дома!)
+response = requests.post(BASE + 'api/v1/houses/2', json={'latitude': 13.3, 'longitude':12.33, 'family_count':4})
+print(response.json())
+# Создание и добавление нового объекта в класса House (Добавление нового дома!)
+response = requests.post(BASE + 'api/v1/houses/2', json={'latitude': 13.3, 'longitude':12.33, 'family_count':4})
 print(response.json())
