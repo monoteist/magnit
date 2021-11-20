dea55c2aeed005a318cc43e00fbf3e44afd8f0d6
= <i.ezerbaev97@gmail.com>
2021-11-20 22:33:53 +0300

    update tests

--- house/tests.py
+++ house/tests.py
@@ -6,5 +6,5 @@ BASE = 'http://127.0.0.1:5000/'
 response = requests.post(BASE + 'api/v1/houses/2', json={'latitude': 13.3, 'longitude':12.33, 'family_count':4})
 print(response.json())
 # Создание и добавление нового объекта в класса House (Добавление нового дома!)
-response = requests.post(BASE + 'api/v1/houses/2', json={'latitude': 13.3, 'longitude':12.33, 'family_count':4})
+response = requests.post(BASE + 'api/v1/houses/5', json={'latitude': 13.3, 'longitude':12.33, 'family_count':4})
 print(response.json())
